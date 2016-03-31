a908e2854f8cef90d5717963bd3c5756387de342
MiddleManager <hjaltewallin@gmail.com>
2016-03-31 13:01:25 +0200

    Initial commit

--- CH13/ex13_1.py
+++ CH13/ex13_1.py
@@ -1,9 +1,21 @@
+# Declarations
 old_file = open("words.txt", "r")
-content = old_file.read()
-
 new_file = open("new_words.txt", "w")
+content = []
 
-new_file.write(content[::-1])
+# Reads all the lines into a list
+while True:
+    line = old_file.readline()
+
+    if line == "":
+        break
+
+    content.append(line)
+
+# Writes those lines in reverse to the new file
+for i in range(len(content)-1, -1, -1):
+    print(i)
+    new_file.write(content[i])
 
 new_file.close()
 old_file.close()
