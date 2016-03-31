e42047d0976b1f1567dd90343e6407e4233e8786
MiddleManager <hjaltewallin@gmail.com>
2016-03-31 18:18:07 +0200

    final functunality

--- CH13/ex13_2.py
+++ CH13/ex13_2.py
@@ -1,3 +1,14 @@
 # Write a program that reads a file and prints only those lines that contain the substring snake.
 
 new_file = open("words.txt", "r")
+
+while True:
+    line = new_file.readline()
+
+    if line == "":
+        break
+
+    if "Snakes" in line:
+        print(line)
+
+new_file.close()
